8a76534e901c1b9b2721e15e81faa2457af238b7
faizasidd <faiza.siddiqi1@gmail.com>
2022-05-12 13:15:12 -0400

    Added CommentItem component and styled it

--- src/components/CommentItem/CommentItem.js
+++ src/components/CommentItem/CommentItem.js
@@ -5,11 +5,9 @@ const CommentItem = (props) => {
   
     return (
       <div className='comment'>
-        {
-          props.src
-            ? <img className='comment__img' src={props.src} alt={'The user\'s profile'}/> 
-            : <div className='comment__img'></div>
-        }
+       
+            <div className='comment__img'></div>
+        
         <h3 className='comment__name'>{props.name}</h3>
         <h3 className='comment__timestamp'>{props.timestamp}</h3>
         <p className='comment__text'>{props.comment}</p>
