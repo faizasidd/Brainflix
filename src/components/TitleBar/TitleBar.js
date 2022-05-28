94dbb0d88ce687702815e1ff6c056b7771467a2a
faizasidd <faiza.siddiqi1@gmail.com>
2022-05-27 21:55:31 -0400

    Added route/links to TitleBar component

--- src/components/TitleBar/TitleBar.js
+++ src/components/TitleBar/TitleBar.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import Logo from '../../assets/images/logo/BrainFlix-logo.svg';
 import SearchIcon from '../../assets/images/Icons/search.svg';
@@ -7,42 +8,55 @@ import './TitleBar.scss';
 
 // The title bar at the top of the page
 
-const TitleBar = (props) => {
-  return (
-    <div className='title-bar'>
+class TitleBar extends Component {
 
-      <img
-        className='title-bar__logo'
-        src={Logo}
-        alt='The Brainflix Logo'
-      />
+  render() {
+    return (
+      <div className='title-bar'>
 
-      <div className='title-bar__search-container'>
+        {/* The Brainflix logo that redirects to the vidoes page */}
+        <Route render={({ brainflix }) => (
+          <div
+            className='title-bar__logo-container'
+            onClick={() => brainflix.push('/videos')}
+          >
+            <img
+              className='title-bar__logo'
+              src={Logo}
+              alt='The Brainflix Logo'
+
+            />
+          </div>
+        )} />
+
+        {/* The search bar container for the input and icon */}
+        <div className='title-bar__search-container'>
+          <img
+            className='title-bar__icon'
+            src={SearchIcon}
+            alt='The search icon'
+          />
+          <input
+            className='text-input text-input--search'
+            type='text'
+            placeholder='Search'
+          />
+        </div>
+
+        {/* The upload button that redirects to the upload page */}
+        <Link to='/upload' className='btn btn--upload'>
+          <img src={UploadIcon} alt='The upload icon'/>
+          upload
+        </Link>
+
         <img
-          className='title-bar__icon'
-          src={SearchIcon}
-          alt='The search icon'
-        />
-        <input
-          className='title-bar__search'
-          type='text'
-          placeholder='Search'
+          className='title-bar__img'
+          src={this.props.image}
+          alt={'The user\'s profile'}
         />
       </div>
-
-      <button className='btn btn--upload'>
-        <img src={UploadIcon} alt='The upload icon'/>
-        upload
-      </button>
-
-      <img
-        className='title-bar__img'
-        src={props.image}
-        alt={'The user\'s profile'}
-      />
-
-    </div>
-  );
+    );
+  }
 }
 
 export default TitleBar;
